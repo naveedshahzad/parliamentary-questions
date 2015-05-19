77de04bc47f194b14d6f872f52381f1614c98fc4
Lukasz Raczylo <lukasz@raczylo.com>
2015-05-19 12:44:31 +0100

    Multiple changes to make pq project build using current MoJ images.

--- docker/rails/run.sh
+++ docker/rails/run.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
+# docker run -p 80 -p 22 --env LOGSTASH_SERVER=54.76.245.119:2510 --env GRAPHITE_SERVER=54.76.245.119 -d docker.local:5000/myrails /sbin/my_init --enable-insecure-key
 
-docker run -p 80 -p 22 --env LOGSTASH_SERVER=54.76.245.119:2510 --env GRAPHITE_SERVER=54.76.245.119 -d docker.local:5000/myrails /sbin/my_init --enable-insecure-key
+set -e
+echo "inside run.sh"
+cd /rails
+
+case ${DOCKER_STATE} in
+migrate)
+    echo "running migrate"
+    bundle exec rake db:migrate
+    ;;
+seed)
+    echo "running seed"
+    bundle exec rake db:drop
+    bundle exec rake db:create
+    bundle exec rake db:migrate
+    bundle exec rake db:seed
+    bundle exec rake claims:demo_data
+    ;;
+esac
+exec bundle exec unicorn -p 80
