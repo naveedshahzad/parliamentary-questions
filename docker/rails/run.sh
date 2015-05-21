37aa821388f9ab405e5e0268feb34f2b91d0de84
Lukasz Raczylo <lukasz@raczylo.com>
2015-05-21 16:51:21 +0100

    .

--- docker/rails/run.sh
+++ docker/rails/run.sh
@@ -2,7 +2,6 @@
 set -e
 # docker run -p 80 -p 22 --env LOGSTASH_SERVER=54.76.245.119:2510 --env GRAPHITE_SERVER=54.76.245.119 -d docker.local:5000/myrails /sbin/my_init --enable-insecure-key
 echo "inside run.sh"
-cd /rails
 
 case ${DOCKER_STATE} in
 migrate)
