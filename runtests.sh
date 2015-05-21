876b1584d861d91e1ae54ac45433f9564cc67982
Lukasz Raczylo <lukasz@raczylo.com>
2015-05-21 15:33:45 +0100

    .

--- runtests.sh
+++ runtests.sh
@@ -3,5 +3,6 @@ set -ex
 # Script executing all the test tasks.
 bundle exec rake db:migrate
 npm install phantomjs
+export PATH=$PATH:/usr/src/app/node_modules/phantomjs/lib/phantom/bin
 bundle exec rspec spec
 bundle exec rspec features
